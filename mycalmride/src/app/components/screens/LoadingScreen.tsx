cd594e6ed3bc8e2df0cb28e9fd32093d62fa0513
kimchampion <kimchampion.mobile@gmail.com>
2026-05-03 13:48:15 -0700

    Update LoadingScreen.tsx
    fixed comments

--- mycalmride/src/app/components/screens/LoadingScreen.tsx
+++ mycalmride/src/app/components/screens/LoadingScreen.tsx
@@ -57,7 +57,7 @@ export function LoadingScreen() {
 
       {/* Home Indicator */}
       <div className="absolute left-0 bottom-0 w-full flex items-end justify-center pb-[7.86px]">
-       <!-- <!div className="bg-white h-[4.913px] rounded-[100px] w-[141.48px]" /> -->
+        {/* <!div className="bg-white h-[4.913px] rounded-[100px] w-[141.48px]" /> *}
       </div>
     </div>
   );
