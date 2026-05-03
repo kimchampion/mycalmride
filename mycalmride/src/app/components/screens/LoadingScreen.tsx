164b121ad9783ed565dcd51cc994ad581feec8b8
kimchampion <kimchampion.mobile@gmail.com>
2026-05-03 13:50:17 -0700

    Update LoadingScreen.tsx
    fixed error

--- mycalmride/src/app/components/screens/LoadingScreen.tsx
+++ mycalmride/src/app/components/screens/LoadingScreen.tsx
@@ -20,18 +20,30 @@ export function LoadingScreen() {
         <div className="flex flex-col h-[54px] items-start pt-[21px]">
           <div className="flex items-center justify-between w-full">
             <div className="flex-1">
-
+              <p className="font-['SF_Pro:Semibold',sans-serif] font-[590] leading-[22px] text-[17px] text-white whitespace-nowrap" style={{ fontVariationSettings: "'wdth' 100" }}>
+                9:41
+              </p>
             </div>
             <div className="h-[10px] w-[124px]" />
             <div className="flex-1 flex items-center justify-end gap-[7px]">
               <div className="h-[12.226px] w-[19.2px]">
-
+                <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 19.2 12.2264">
+                  <path clipRule="evenodd" d={svgPaths.p1e09e400} fill="white" fillRule="evenodd" />
+                </svg>
               </div>
               <div className="h-[12.328px] w-[17.142px]">
-
+                <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17.1417 12.3283">
+                  <path clipRule="evenodd" d={svgPaths.p18b35300} fill="white" fillRule="evenodd" />
+                </svg>
               </div>
               <div className="h-[13px] w-[27.328px]">
-
+                <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 27.328 13">
+                  <g>
+                    <rect height="12" opacity="0.35" rx="3.8" stroke="white" width="24" x="0.5" y="0.5" />
+                    <path d={svgPaths.p3bbd9700} fill="white" opacity="0.4" />
+                    <rect fill="white" height="9" rx="2.5" width="21" x="2" y="2" />
+                  </g>
+                </svg>
               </div>
             </div>
           </div>
@@ -57,7 +69,7 @@ export function LoadingScreen() {
 
       {/* Home Indicator */}
       <div className="absolute left-0 bottom-0 w-full flex items-end justify-center pb-[7.86px]">
-        {/* <!div className="bg-white h-[4.913px] rounded-[100px] w-[141.48px]" /> *}
+        {/* <div className="bg-white h-[4.913px] rounded-[100px] w-[141.48px]" /> */}
       </div>
     </div>
   );
